d632c363fcecd2f1f4c40830e00cd43b79c96af0
Benjamin Tan <tanbenjamin123@gmail.com>
2023-09-22 16:32:49 -0400

    Split files

---
+++ enums.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from enum import Enum
+
+class UnitType(Enum):
+    """Every unit type."""
+    AI = 0
+    Tech = 1
+    Virus = 2
+    Program = 3
+    Firewall = 4
+
+class Player(Enum):
+    """The 2 players."""
+    Attacker = 0
+    Defender = 1
+
+    def next(self) -> Player:
+        """The next (other) player."""
+        if self is Player.Attacker:
+            return Player.Defender
+        else:
+            return Player.Attacker
+
+class GameType(Enum):
+    AttackerVsDefender = 0
+    AttackerVsComp = 1
+    CompVsDefender = 2
+    CompVsComp = 3
---
+++ stats.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+from dataclasses import dataclass, field
+
+@dataclass(slots=True)
+class Stats:
+    """Representation of the global game statistics."""
+    evaluations_per_depth : dict[int,int] = field(default_factory=dict)
+    total_seconds: float = 0.0
