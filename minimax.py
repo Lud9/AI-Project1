3b1936ca292b1ff00853aef7a790a5791f5b91f9
aloisorozco <alois.orozco@gmail.com>
2023-10-03 18:48:27 -0400

    added more functions (not implemented yet)
    todo:
    -implement functions
    -add more complex functions for the e(n) function (watching chess AI vids atm will find out what to mplement)

--- minimax.py
+++ minimax.py
@@ -10,6 +10,7 @@ def hasAttackerWon(game: Game) -> bool:
 def hasDefenderWon(game: Game) -> bool:
     return game.has_winner() is not None and game.has_winner() == Player.Defender
 
+#getting health points
 def getHealthPoints(game: Game) -> int:
     score = 0
     for i in range(len(game.board)):
@@ -23,6 +24,28 @@ def getHealthPoints(game: Game) -> int:
                 score -= curUnit.health
     return score
 
+'''
+similar to health points but also have to account for number of pieces
+since 9 total health points with only 1 piece is worse than 9 total health points with 3 pieces
+'''
+def getNumberOfPieces(game: Game) -> int:
+    return 0
+
+#similar to chess where a queen(8) is given more points than a pawn(1) since it is more valuable
+def getAttackerPiecePoints(game: Game) -> int:
+    return 0
+
+#positive value but it gets substracted after from the score
+def getDefenderPiecePoints(game: Game) -> int:
+    return 0
+
+'''
+2 different functions for attacker & defender piece points because
+attacker values some pieces more than others compared to defender
+'''
+def getPiecePoints(game: Game) -> int:
+    return getAttackerPiecePoints(game) - getDefenderPiecePoints(game)
+
 #during each main loop check game state by calling this function to evaluate e(n)
 def eScore(game: Game) -> int:
     if hasAttackerWon(game):
@@ -33,15 +56,13 @@ def eScore(game: Game) -> int:
     else:
         score = 0
 
-        #getting health points (base heuristic)
         score += getHealthPoints(game)
+        score += getNumberOfPieces(game)
+        score += getPiecePoints(game)
 
         #if more than 3 quarters in, give advantage to defender
         if game.turns_played > (3 * game.options.max_turns/4):
             score -= game.turns_played - (3 * game.options.max_turns/4)
-
-        
-
 
 
     return 0
